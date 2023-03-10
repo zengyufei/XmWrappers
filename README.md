45652a1c3116c9174d772d183eca630d662f8423
zengyufei <31636208@qq.com>
2023-03-10 16:15:38 +0800

    提交README文件

---
+++ README.md
@@ -0,0 +1,2 @@
+![APIFOX](https://raw.github.com/zengyufei/XmWrappers/apifox.png)
+![houduan](https://raw.github.com/zengyufei/XmWrappers/houduan.png)
--- src/main/java/com/example/demo/common/base/BaseEntity.java
+++ src/main/java/com/example/demo/common/base/BaseEntity.java
@@ -1,6 +1,8 @@
 package com.example.demo.common.base;
 
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -10,8 +12,11 @@ import java.util.List;
 public class BaseEntity implements Serializable {
     private static final long serialVersionUID = -80173182440175475L;
 
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @TableField(exist = false)
     List<Condition> conditions;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @TableField(exist = false)
     private List<OrderBy> orders;
 
